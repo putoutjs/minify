86991ce7b1103a6628d46721c3dd3e617ff71cc6
coderaiser <coderaiser@cloudcmd.io>
2023-06-02 11:38:22 +0300

    feature: package: @putout/plugin-minify v1.31.0

--- test/fixture/dead-code-fix.js
+++ test/fixture/dead-code-fix.js
@@ -1,1 +1,1 @@
-'use strict';function a(){var x=1}function c(){var x=f()}function d(){console.log(1);function _a(){return 5}}export{a as foo,c as bar,d as baz};
+'use strict';function a(){var x=1}function c(){var x=f()}function e(){console.log(1);function _c(){return 5}}export{a as foo,c as bar,e as baz};
